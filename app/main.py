6df08abfe137fb9b858c08fbcc4f21be39bdc999
Ivan Noleto <ivannoleto@hotmail.com>
2022-05-31 01:15:33 -0300

    Creating routers

--- app/main.py
+++ app/main.py
@@ -2,7 +2,11 @@
 
 from fastapi import FastAPI
 
+from .routers import articles
+
 app = FastAPI()
+
+app.include_router(articles.router)
 
 
 @app.get("/")
---
+++ app/routers/articles.py
@@ -0,0 +1,37 @@
+"""Routers for Articles."""
+
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/articles/{id}")
+async def get_article():
+    """Function to get articles from the space flightnews API"""
+
+    return {"message": "Get article is not supported yet."}
+
+
+@router.get("/articles/")
+async def get_articles():
+    """Function to get articles from the space flightnews API"""
+
+    return {"message": "Get articles is not supported yet."}
+
+
+@router.post("/articles/")
+async def post_article():
+    """Function to post articles from the space flightnews API"""
+    return {"message": "Posting articles is not supported yet."}
+
+
+@router.put("/articles/{id}")
+async def put_article():
+    """Function to put articles from the space flightnews API"""
+    return {"message": "Putting articles is not supported yet."}
+
+
+@router.delete("/articles/{id}")
+async def delete_article():
+    """Function to delete articles from the space flightnews API"""
+    return {"message": "Deleting articles is not supported yet."}
